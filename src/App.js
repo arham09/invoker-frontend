1a1aad3df9b90e741734082e2b9f1a464b83e6f7
arham09 <arham.abiyan@gmail.com>
2019-04-06 21:28:13 +0700

    Fresh react

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            First React App by Arham Awal Abiyan
+          </p>
+          <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
